f791f32ac94ec36a9e4817472eed132f60bd7dbc
kalayn <kalyan>
2026-01-23 10:21:00 -0600

    Initial Commit

---
+++ kalayn/kalaynApp.swift
@@ -0,0 +1,17 @@
+//
+//  kalaynApp.swift
+//  kalayn
+//
+//  Created by kalyan on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct kalaynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
