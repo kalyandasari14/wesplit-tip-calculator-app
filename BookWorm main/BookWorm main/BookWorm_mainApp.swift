823fdfd7f511181b128b0525ae48e314b771de7e
kalayn <kalyan>
2026-02-19 11:42:00 -0600

    commit all clear everything

---
+++ BookWorm main/BookWorm main/BookWorm_mainApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWorm_mainApp.swift
+//  BookWorm main
+//
+//  Created by kalyan on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct BookWorm_mainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
